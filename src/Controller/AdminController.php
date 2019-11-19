0881e3a8b08c48a24edcdbaa8d9a42c6371b0eca
Alexandr Safonov <safonov.alexandr.89@gmail.com>
2019-11-19 16:28:18 +0200

    add authorization

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends Controller
+{
+    public function index(Request $request) 
+    {   
+        
+        return $this->render('pages/admin.html.twig', []);
+    }
+}
